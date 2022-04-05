cbb3d6d7302404ed68ed31fd3410c982b1129d48
Przemyslaw Damek <pdamek.bb@gmail.com>
2022-04-05 11:08:45 +0200

    Show and hide basket

--- src/js/script.js
+++ src/js/script.js
@@ -282,6 +282,39 @@
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+
+      });
+    
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -293,13 +326,20 @@
       }
     },
 
-    initData: function () {
+    initData: function (){
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
 
-    init: function () {
+    initCart: function (){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
+    init: function (){
       const thisApp = this;
       //console.log('*** App starting ***');
       //console.log('thisApp:', thisApp);
@@ -308,6 +348,7 @@
       //console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
